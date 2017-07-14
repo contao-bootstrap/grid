bc859a326b20ac57038afd3b0119ba66e59437a4
David Molineus <david.molineus@netzmacht.de>
2017-07-14 12:29:33 +0200

    Add translations for module group headings.

--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -16,5 +16,7 @@ $GLOBALS['TL_LANG']['tl_module']['bs_gridModules'][0]          = 'Modules';
 $GLOBALS['TL_LANG']['tl_module']['bs_gridModules'][1]          = 'Choose the modules and columns you want to combine.';
 $GLOBALS['TL_LANG']['tl_module']['bs_gridModule'][0]           = 'Module or column';
 $GLOBALS['TL_LANG']['tl_module']['bs_gridModule'][1]           = 'Choose a module or define next column.';
+$GLOBALS['TL_LANG']['tl_module']['bs_gridModule']['grid']      = 'Grid';
+$GLOBALS['TL_LANG']['tl_module']['bs_gridModule']['module']    = 'Module';
 $GLOBALS['TL_LANG']['tl_module']['bs_gridModules_inactive'][0] = 'Inactive';
 $GLOBALS['TL_LANG']['tl_module']['bs_gridModules_inactive'][1] = 'Deactivate entry temporary.';
