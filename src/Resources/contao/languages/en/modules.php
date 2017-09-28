53582ce9ea23042a8683b4df5f6325faa2dc0a31
David Molineus <david.molineus@netzmacht.de>
2017-09-28 13:14:22 +0200

    Translate new labels.

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -19,6 +19,8 @@ $GLOBALS['TL_LANG']['CTE']['bs_gridSeparator'][0] = 'Grid Separator';
 $GLOBALS['TL_LANG']['CTE']['bs_gridSeparator'][1] = 'Grid Separator Element';
 $GLOBALS['TL_LANG']['CTE']['bs_gridStop'][0]      = 'Grid Stop';
 $GLOBALS['TL_LANG']['CTE']['bs_gridStop'][1]      = 'Grid Stop Element';
+$GLOBALS['TL_LANG']['CTE']['bs_grid_gallery'][0]  = 'Gallery (Bootstrap grid)';
+$GLOBALS['TL_LANG']['CTE']['bs_grid_gallery'][1]  = 'Gallery using Bootstrap grid';
 
 $GLOBALS['TL_LANG']['FMD']['bs_grid'][0] = 'Grid';
 $GLOBALS['TL_LANG']['FMD']['bs_grid'][1] = 'Bootstrap Grid module.';
--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -15,8 +15,10 @@ $GLOBALS['TL_LANG']['tl_content']['bs_grid_wizard_legend'] = 'Wizard';
 
 
 $GLOBALS['TL_LANG']['tl_content']['bs_grid'][0]        = 'Grid';
-$GLOBALS['TL_LANG']['tl_content']['bs_grid'][1]        = 'Please choose the defined grid definition.';
+$GLOBALS['TL_LANG']['tl_content']['bs_grid'][1]        = 'Please choose a grid definition.';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_name'][0]   = 'Grid name';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_name'][1]   = 'Internal grid name used for the backend view.';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_wizard'][0] = 'Create separators';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_wizard'][1] = 'Choose how many separators should be automatically created.';
+$GLOBALS['TL_LANG']['tl_content']['bs_image_sizes'][0] = 'Image sizes';
+$GLOBALS['TL_LANG']['tl_content']['bs_image_sizes'][1] = 'You can define a series of images sizes which are used iteratively.';
