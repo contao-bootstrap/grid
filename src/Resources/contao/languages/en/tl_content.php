041cf622882d67b014e96fdddde9f3caa6e189a0
David Molineus <david.molineus@netzmacht.de>
2018-07-27 16:56:08 +0200

    Add missing grid parent translation.

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -16,6 +16,8 @@ $GLOBALS['TL_LANG']['tl_content']['bs_grid_wizard_legend'] = 'Wizard';
 
 $GLOBALS['TL_LANG']['tl_content']['bs_grid'][0]        = 'Grid';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid'][1]        = 'Please choose a grid definition.';
+$GLOBALS['TL_LANG']['tl_content']['bs_grid_parent'][1] = 'Parent element';
+$GLOBALS['TL_LANG']['tl_content']['bs_grid_parent'][1] = 'Please select the parent element.';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_name'][0]   = 'Grid name';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_name'][1]   = 'Internal grid name used for the backend view.';
 $GLOBALS['TL_LANG']['tl_content']['bs_grid_wizard'][0] = 'Create separators';
--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -20,9 +20,11 @@ $GLOBALS['TL_LANG']['FFL']['bs_gridSeparator'][1] = 'Bootstrap grid separator fo
 
 $GLOBALS['TL_LANG']['tl_form_field']['bs_grid_wizard_legend'] = 'Wizard';
 
-$GLOBALS['TL_LANG']['tl_form_field']['bs_grid'][0]      = 'Grid';
-$GLOBALS['TL_LANG']['tl_form_field']['bs_grid'][1]      = 'Please choose the defined grid definition.';
-$GLOBALS['TL_LANG']['tl_form_field']['bs_grid_name'][0] = 'Grid name';
-$GLOBALS['TL_LANG']['tl_form_field']['bs_grid_name'][1] = 'Internal grid name used for the backend view.';
+$GLOBALS['TL_LANG']['tl_form_field']['bs_grid'][0]        = 'Grid';
+$GLOBALS['TL_LANG']['tl_form_field']['bs_grid'][1]        = 'Please choose the defined grid definition.';
+$GLOBALS['TL_LANG']['tl_form_field']['bs_grid_parent'][1] = 'Parent element';
+$GLOBALS['TL_LANG']['tl_form_field']['bs_grid_parent'][1] = 'Please select the parent element.';
+$GLOBALS['TL_LANG']['tl_form_field']['bs_grid_name'][0]   = 'Grid name';
+$GLOBALS['TL_LANG']['tl_form_field']['bs_grid_name'][1]   = 'Internal grid name used for the backend view.';
 $GLOBALS['TL_LANG']['tl_form_field']['bs_grid_wizard'][0] = 'Create separators';
 $GLOBALS['TL_LANG']['tl_form_field']['bs_grid_wizard'][1] = 'Choose how many separators should be automatically created.';
