ca6624fa1e0880a9ec31b650a5f10b97def5114b
David Molineus <david.molineus@netzmacht.de>
2023-06-09 16:09:30 +0200

    Update translations

--- src/Resources/contao/languages/en/tl_bs_grid.php
+++ src/Resources/contao/languages/en/tl_bs_grid.php
@@ -1,88 +1,74 @@
 <?php
 
 /**
- * Contao Bootstrap grid.
+ * Translations are managed automatically using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
  *
- * @package    contao-bootstrap
- * @subpackage Grid
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2017-2020 netzmacht David Molineus. All rights reserved.
- * @license    https://github.com/contao-bootstrap/grid/blob/master/LICENSE LGPL 3.0-or-later
- * @filesource
+ * Last-updated: 2023-06-09T14:03:43+00:00
  */
 
-$GLOBALS['TL_LANG']['tl_bs_grid']['title_legend'] = 'Title and description';
-$GLOBALS['TL_LANG']['tl_bs_grid']['grid_legend']  = 'Grid sizes and columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['row_legend']   = 'Row configuration';
-
-$GLOBALS['TL_LANG']['tl_bs_grid']['new'][0]    = 'New grid definition';
-$GLOBALS['TL_LANG']['tl_bs_grid']['new'][1]    = 'Create new grid definition';
-$GLOBALS['TL_LANG']['tl_bs_grid']['edit'][0]   = 'Edit grid definition';
-$GLOBALS['TL_LANG']['tl_bs_grid']['edit'][1]   = 'Edit grid definition ID "%s"';
-$GLOBALS['TL_LANG']['tl_bs_grid']['copy'][0]   = 'Copy grid definition';
-$GLOBALS['TL_LANG']['tl_bs_grid']['copy'][1]   = 'Copy grid definition ID "%s"';
-$GLOBALS['TL_LANG']['tl_bs_grid']['delete'][0] = 'Delete grid definition';
-$GLOBALS['TL_LANG']['tl_bs_grid']['delete'][1] = 'Delete grid definition ID "%s"';
-$GLOBALS['TL_LANG']['tl_bs_grid']['show'][0]   = 'Show details';
-$GLOBALS['TL_LANG']['tl_bs_grid']['show'][1]   = 'Show details of grid definition ID "%s"';
-
-$GLOBALS['TL_LANG']['tl_bs_grid']['title'][0]       = 'Title';
-$GLOBALS['TL_LANG']['tl_bs_grid']['title'][1]       = 'Title of the grid definition.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['description'][0] = 'Description';
-$GLOBALS['TL_LANG']['tl_bs_grid']['description'][1] = 'Description of the grid definition.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['sizes'][0]       = 'Sizes';
-$GLOBALS['TL_LANG']['tl_bs_grid']['sizes'][1]       = 'Please select which sizes should be used.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xs'][0]          = 'Extra small';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xs'][1]          = 'Extra small (xs). Default bootstrap: <576px.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['sm'][0]          = 'Small';
-$GLOBALS['TL_LANG']['tl_bs_grid']['sm'][1]          = 'Small (sm). Default bootstrap: ≥576px.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['md'][0]          = 'Medium';
-$GLOBALS['TL_LANG']['tl_bs_grid']['md'][1]          = 'Medium (md). Default bootstrap: ≥768px.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['lg'][0]          = 'Large';
-$GLOBALS['TL_LANG']['tl_bs_grid']['lg'][1]          = 'Large (lg). Default bootstrap: ≥992px';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xl'][0]          = 'Extra large';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xl'][1]          = 'Extra large (xl). Default bootstrap: ≥1200px';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xxl'][0]         = 'Extra extra large';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xxl'][1]         = 'Extra extra large (xxl). Default bootstrap: ≥1400px';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xsSize'][0]      = 'Extra small columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xsSize'][1]      = 'Please define column sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['smSize'][0]      = 'Small columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['smSize'][1]      = 'Please define column sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['mdSize'][0]      = 'Medium columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['mdSize'][1]      = 'Please define column sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['lgSize'][0]      = 'Large columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['lgSize'][1]      = 'Please define column sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xlSize'][0]      = 'Extra large columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xlSize'][1]      = 'Please define column sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xxlSize'][0]     = 'Extra extra large columns';
-$GLOBALS['TL_LANG']['tl_bs_grid']['xxlSize'][1]     = 'Please define column sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['align'][0]       = 'Vertical alignment';
-$GLOBALS['TL_LANG']['tl_bs_grid']['align'][1]       = 'Use flexbox alignment utilities to vertically align columns.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['justify'][0]     = 'Horizontal alignment';
-$GLOBALS['TL_LANG']['tl_bs_grid']['justify'][1]     = 'Use flexbox horizontal alignment utilities to horizontally align columns.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['rowClass'][0]    = 'Row classes';
-$GLOBALS['TL_LANG']['tl_bs_grid']['rowClass'][1]    = 'Add custom css classes to the row.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['noGutters'][0]   = 'No gutters';
-$GLOBALS['TL_LANG']['tl_bs_grid']['noGutters'][1]   = 'Remove gutters between columns.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['width'][0]       = 'Width';
-$GLOBALS['TL_LANG']['tl_bs_grid']['width'][1]       = 'Column width.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['offset'][0]      = 'Offset';
-$GLOBALS['TL_LANG']['tl_bs_grid']['offset'][1]      = 'Column offset.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['order'][0]       = 'Order';
-$GLOBALS['TL_LANG']['tl_bs_grid']['order'][1]       = 'Manipulate order with push and pull classes or flexbox utilities.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['align'][0]       = 'Alignment';
-$GLOBALS['TL_LANG']['tl_bs_grid']['align'][1]       = 'Vertical alignment with flexbox utilities.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['class'][0]       = 'Classes';
-$GLOBALS['TL_LANG']['tl_bs_grid']['class'][1]       = 'Custom css classes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['reset'][0]       = 'Reset';
-$GLOBALS['TL_LANG']['tl_bs_grid']['reset'][1]       = 'Reset position before the column.';
-
-$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['1'][0]       = 'all';
-$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['1'][1]       = 'Reset all upcoming sizes.';
-$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['2'][0]       = 'size';
-$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['2'][1]       = 'Reset only this size.';
-
-$GLOBALS['TL_LANG']['tl_content']['bs_image_size_width'][0]  = 'Width';
-$GLOBALS['TL_LANG']['tl_content']['bs_image_size_width'][1]  = 'Width of the image';
-$GLOBALS['TL_LANG']['tl_content']['bs_image_size_height'][0] = 'Height';
-$GLOBALS['TL_LANG']['tl_content']['bs_image_size_height'][1] = 'Height of the image';
+$GLOBALS['TL_LANG']['tl_bs_grid']['align']['0']                = 'Alignment';
+$GLOBALS['TL_LANG']['tl_bs_grid']['align']['1']                = 'Vertical alignment with flexbox utilities.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['class']['0']                = 'Classes';
+$GLOBALS['TL_LANG']['tl_bs_grid']['class']['1']                = 'Custom css classes.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['copy']['0']                 = 'Copy grid definition';
+$GLOBALS['TL_LANG']['tl_bs_grid']['copy']['1']                 = 'Copy grid definition ID "%s"';
+$GLOBALS['TL_LANG']['tl_bs_grid']['delete']['0']               = 'Delete grid definition';
+$GLOBALS['TL_LANG']['tl_bs_grid']['delete']['1']               = 'Delete grid definition ID "%s"';
+$GLOBALS['TL_LANG']['tl_bs_grid']['description']['0']          = 'Description';
+$GLOBALS['TL_LANG']['tl_bs_grid']['description']['1']          = 'Description of the grid definition.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['edit']['0']                 = 'Edit grid definition';
+$GLOBALS['TL_LANG']['tl_bs_grid']['edit']['1']                 = 'Edit grid definition ID "%s"';
+$GLOBALS['TL_LANG']['tl_bs_grid']['grid_legend']               = 'Grid sizes and columns';
+$GLOBALS['TL_LANG']['tl_bs_grid']['justify']['0']              = 'Horizontal alignment';
+$GLOBALS['TL_LANG']['tl_bs_grid']['justify']['1']              = 'Use flexbox horizontal alignment utilities to horizontally align columns.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['lg']['0']                   = 'Large';
+$GLOBALS['TL_LANG']['tl_bs_grid']['lg']['1']                   = 'Large (lg). Default bootstrap: ≥992px';
+$GLOBALS['TL_LANG']['tl_bs_grid']['lgSize']['0']               = 'Large columns';
+$GLOBALS['TL_LANG']['tl_bs_grid']['lgSize']['1']               = 'Please define column sizes.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['md']['0']                   = 'Medium';
+$GLOBALS['TL_LANG']['tl_bs_grid']['md']['1']                   = 'Medium (md). Default bootstrap: ≥768px.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['mdSize']['0']               = 'Medium columns';
+$GLOBALS['TL_LANG']['tl_bs_grid']['mdSize']['1']               = 'Please define column sizes.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['new']['0']                  = 'New grid definition';
+$GLOBALS['TL_LANG']['tl_bs_grid']['new']['1']                  = 'Create new grid definition';
+$GLOBALS['TL_LANG']['tl_bs_grid']['noGutters']['0']            = 'No gutters';
+$GLOBALS['TL_LANG']['tl_bs_grid']['noGutters']['1']            = 'Remove gutters between columns.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['offset']['0']               = 'Offset';
+$GLOBALS['TL_LANG']['tl_bs_grid']['offset']['1']               = 'Column offset.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['order']['0']                = 'Order';
+$GLOBALS['TL_LANG']['tl_bs_grid']['order']['1']                = 'Manipulate order with push and pull classes or flexbox utilities.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['reset']['0']                = 'Reset';
+$GLOBALS['TL_LANG']['tl_bs_grid']['reset']['1']                = 'Reset position before the column.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['1']['0']          = 'all';
+$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['1']['1']          = 'Reset all upcoming sizes.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['2']['0']          = 'size';
+$GLOBALS['TL_LANG']['tl_bs_grid']['resets']['2']['1']          = 'Reset only this size.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['rowClass']['0']             = 'Row classes';
+$GLOBALS['TL_LANG']['tl_bs_grid']['rowClass']['1']             = 'Add custom css classes to the row.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['row_legend']                = 'Row configuration';
+$GLOBALS['TL_LANG']['tl_bs_grid']['show']['0']                 = 'Show details';
+$GLOBALS['TL_LANG']['tl_bs_grid']['show']['1']                 = 'Show details of grid definition ID "%s"';
+$GLOBALS['TL_LANG']['tl_bs_grid']['sizes']['0']                = 'Sizes';
+$GLOBALS['TL_LANG']['tl_bs_grid']['sizes']['1']                = 'Please select which sizes should be used.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['sm']['0']                   = 'Small';
+$GLOBALS['TL_LANG']['tl_bs_grid']['sm']['1']                   = 'Small (sm). Default bootstrap: ≥576px.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['smSize']['0']               = 'Small columns';
+$GLOBALS['TL_LANG']['tl_bs_grid']['smSize']['1']               = 'Please define column sizes.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['title']['0']                = 'Title';
+$GLOBALS['TL_LANG']['tl_bs_grid']['title']['1']                = 'Title of the grid definition.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['title_legend']              = 'Title and description';
+$GLOBALS['TL_LANG']['tl_bs_grid']['width']['0']                = 'Width';
+$GLOBALS['TL_LANG']['tl_bs_grid']['width']['1']                = 'Column width.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xl']['0']                   = 'Extra large';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xl']['1']                   = 'Extra large (xl). Default bootstrap: ≥1200px';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xlSize']['0']               = 'Extra large columns';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xlSize']['1']               = 'Please define column sizes.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xs']['0']                   = 'Extra small';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xs']['1']                   = 'Extra small (xs). Default bootstrap: <576px.';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xsSize']['0']               = 'Extra small columns';
+$GLOBALS['TL_LANG']['tl_bs_grid']['xsSize']['1']               = 'Please define column sizes.';
+$GLOBALS['TL_LANG']['tl_content']['bs_image_size_height']['0'] = 'Height';
+$GLOBALS['TL_LANG']['tl_content']['bs_image_size_height']['1'] = 'Height of the image';
+$GLOBALS['TL_LANG']['tl_content']['bs_image_size_width']['0']  = 'Width';
+$GLOBALS['TL_LANG']['tl_content']['bs_image_size_width']['1']  = 'Width of the image';
--- src/Resources/contao/languages/en/tl_theme.php
+++ src/Resources/contao/languages/en/tl_theme.php
@@ -1,18 +1,18 @@
 <?php
 
-declare(strict_types=1);
+/**
+ * Translations are managed automatically using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * Last-updated: 2023-06-09T13:58:47+00:00
+ */
 
-// Operations
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid'][0] = 'Grid definitions';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid'][1] = 'Manage grid definitions';
-
-// Legends
-$GLOBALS['TL_LANG']['tl_theme']['bootstrap_legend'] = 'Bootstrap';
-
-// Fields
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns'][0]      = 'Grid columns';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns'][1]      = 'Override the number of grid columns (default 12).';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes'][0]        = 'Grid sizes';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes'][1]        = 'Define the size for your grid (Default: xs, sm, md, lg, xl and xxl).';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size'][0] = 'Default grid size';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size'][1] = 'Define the default size for your grid (Default is xs).';
+$GLOBALS['TL_LANG']['tl_theme']['bootstrap_legend']          = 'Bootstrap';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid']['0']              = 'Grid definitions';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid']['1']              = 'Manage grid definitions';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns']['0']      = 'Grid columns';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns']['1']      = 'Override the number of grid columns (default 12).';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size']['0'] = 'Default grid size';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size']['1'] = 'Define the default size for your grid (Default is xs).';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes']['0']        = 'Grid sizes';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes']['1']        = 'Define the size for your grid (Default: xs, sm, md, lg und xl).';
