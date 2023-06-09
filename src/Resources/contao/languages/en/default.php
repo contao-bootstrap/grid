6e8debba542e11b48011a9a35f5521d5b475d24c
David Molineus <david.molineus@netzmacht.de>
2023-06-09 16:06:22 +0200

    Update english language files

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Contao Bootstrap grid.
- *
- * @package    contao-bootstrap
- * @subpackage Grid
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2017-2020 netzmacht David Molineus. All rights reserved.
- * @license    https://github.com/contao-bootstrap/grid/blob/master/LICENSE LGPL 3.0-or-later
- * @filesource
- */
+declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['ERR']['bsGridParentMissing'] = 'No parent element connected';
--- src/Resources/contao/languages/en/tl_theme.php
+++ src/Resources/contao/languages/en/tl_theme.php
@@ -1,15 +1,6 @@
 <?php
 
-/**
- * Contao Bootstrap grid.
- *
- * @package    contao-bootstrap
- * @subpackage Grid
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2017-2020 netzmacht David Molineus. All rights reserved.
- * @license    https://github.com/contao-bootstrap/grid/blob/master/LICENSE LGPL 3.0-or-later
- * @filesource
- */
+declare(strict_types=1);
 
 // Operations
 $GLOBALS['TL_LANG']['tl_theme']['bs_grid'][0] = 'Grid definitions';
@@ -19,9 +10,9 @@ $GLOBALS['TL_LANG']['tl_theme']['bs_grid'][1] = 'Manage grid definitions';
 $GLOBALS['TL_LANG']['tl_theme']['bootstrap_legend'] = 'Bootstrap';
 
 // Fields
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns'][0] = 'Grid columns';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns'][1] = 'Override the number of grid columns (default 12).';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes']['0'] = 'Grid sizes';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes']['1'] = 'Define the size for your grid (Default: xs, sm, md, lg und xl).';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size']['0'] = 'Default grid size';
-$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size']['1'] = 'Define the default size for your grid (Default is xs).';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns'][0]      = 'Grid columns';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_columns'][1]      = 'Override the number of grid columns (default 12).';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes'][0]        = 'Grid sizes';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_sizes'][1]        = 'Define the size for your grid (Default: xs, sm, md, lg, xl and xxl).';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size'][0] = 'Default grid size';
+$GLOBALS['TL_LANG']['tl_theme']['bs_grid_default_size'][1] = 'Define the default size for your grid (Default is xs).';
